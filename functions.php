24238813c674fc7546123f698b0828cf9ac5b78c
Wayne Graham <wgraham@clir.org>
2017-08-17 15:50:13 -0400

    Fix path for the JavaScript loading for the map path
    #1

--- functions.php
+++ functions.php
@@ -9,5 +9,5 @@ add_action('wp_print_scripts', 'fudge2_child_custom_js');
 function fudge2_child_custom_js(){
   // not sure why there are two of these...
   wp_dequeue_script('fudge-jquery-map');
-  wp_enqueue_script( 'fudge-child-map', get_stylesheet_directory_uri() . '/js/jquery.map.js', array( 'jquery' ), false, false );
+  wp_enqueue_script( 'fudge-child-map', get_stylesheet_directory_uri() . '/assets/js/jquery.map.js', array( 'jquery' ), false, false );
 }
