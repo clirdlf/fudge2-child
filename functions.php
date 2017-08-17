0a1996c38059dc12f3560a2ff6cc412d58d54b30
Wayne Graham <wgraham@clir.org>
2017-08-17 15:16:23 -0400

    Add documentation for the custom JS documentation

--- functions.php
+++ functions.php
@@ -1,7 +1,13 @@
 <?php
 add_action('wp_print_scripts', 'fudge2_child_custom_js');
 
+/**
+ * Enquey the correct jquery-map file
+ *
+ * @see https://showthemes.zendesk.com/hc/en-us/articles/115002214745--Fudge-2-Theme-How-to-adjust-the-zoom-of-the-map-
+ */
 function fudge2_child_custom_js(){
+  // not sure why there are two of these...
   wp_dequeue_script('fudge-jquery-map');
   wp_dequeue_script('fudge-jquery-map');
   wp_enqueue_script( 'fudge-child-map', get_stylesheet_directory_uri() . '/js/jquery.map.js', array( 'jquery' ), false, false );
